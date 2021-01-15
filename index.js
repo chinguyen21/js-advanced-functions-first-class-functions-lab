1617441ee6ebf927bffc6497c37d8e9df1f6b734
Chi Nguyen <nguyen.thimai@northeastern.edu>
2021-01-15 09:35:52 -0500

    commit tree db488877f097784598d82a12870939cbe2d6acad

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution in this file!
+
+const returnFirstTwoDrivers = (drivers) => drivers.slice(0,2)
+
+const returnLastTwoDrivers = (drivers) => drivers.slice(-2)
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+const createFareMultiplier = (int) => (n) => n * int
+
+
+const fareDoubler = (int) => createFareMultiplier(2)(int)
+
+const fareTripler = (int) => createFareMultiplier(3)(int)
+
+
+const selectDifferentDrivers = (drivers, func) => func(drivers)
